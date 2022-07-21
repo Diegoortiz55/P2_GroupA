a3f603971667bfd7def29d02ecaa19250f62b8f7
Jacob T. Linao <106113118+jacobtlin@users.noreply.github.com>
2022-07-21 10:19:50 -0500

    Update scripts.js

--- Jacob - FrontEnd Assets/js/scripts.js
+++ Jacob - FrontEnd Assets/js/scripts.js
@@ -2,7 +2,6 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
